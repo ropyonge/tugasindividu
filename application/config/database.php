ef0ab19223e6fdf100c22ca56465fedd579ffd82
ropyonge <ropykrisnasaputra@gmail.com>
2019-07-02 10:54:10 +0700

    first commit

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'backand',
+	'hostname' => 'ftpupload.net',
+	'username' => 'b8_24101454',
+	'password' => 'bsibsd',
+	'database' => 'perpust',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
